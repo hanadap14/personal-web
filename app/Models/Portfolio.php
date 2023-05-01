eff2890c515af1020d247b5237c3010af35fc5f0
hanadap14 <hana.pertiwi@binus.ac.id>
2023-05-01 21:17:25 +0800

    first

---
+++ app/Models/Portfolio.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Portfolio extends Model
+{
+    use HasFactory;
+
+    protected $table = 'portfolio';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'id',
+        'title',
+        'year',
+        'photo'
+    ];
+}
